8b4de294f368eebe07e28d6dcc0af859372591ab
Samit Koyom <samitkoyom@gmail.com>
2024-12-21 18:24:38 +0700

    initial commit

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,19 @@
+// rfc
+import AboutUs from '@/components/AboutUs'
+import CompanyLogos from '@/components/CompanyLogos'
+import Hero from '@/components/Hero'
+import NewsUpdate from '@/components/NewsUpdate'
+import Resume from '@/components/Resume'
+import React from 'react'
+
+export default function App() {
+  return (
+    <main>
+      <Hero />
+      <CompanyLogos />
+      <AboutUs />
+      <NewsUpdate />
+      <Resume />
+    </main>
+  )
+}
